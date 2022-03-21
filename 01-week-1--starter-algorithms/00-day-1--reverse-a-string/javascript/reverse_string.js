46c2038b5b3a318a815f457ae7390b5e20cd24d9
danainjax <danainjax@gmail.com>
2022-03-21 05:09:29 -0400

    solved js

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,19 +1,32 @@
 function reverseString(str) {
-  // type your code here
+  let newString = ''
+  for (let index = str.length - 1; index >= 0; index--) {
+    newString += str[index]
+  }
+  return newString
 }
+
+// NOTES:
+// solve using iteration, no built in methods
+// 1 . initialize an empty string to store the newString
+// 2. start a for loop with the index equal to the string's length - 1, while the index is  greater than or equal to zero (condition), we decrement the index
+// 3. return the newString
+// So, that means if we input a string "danaisthebest", the length of the string is 13. We start with the string's index of 13 -1, or 12.
+// string[12] would give us the last letter in the string 't', then we add the 't' to the newString variable we are storing
+// on the next iteration the index is decremented to 11. That is the 's', we add the 's' to the newString var and we now have stored, 'ts', and so on until we have danaisthebest reversed : 'tsebehtsianad'
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("Expecting: 'ih'")
+  console.log('=>', reverseString('hi'))
 
-  console.log("");
+  console.log('')
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log("Expecting: 'ybabtac'")
+  console.log('=>', reverseString('catbaby'))
 }
 
-module.exports = reverseString;
+module.exports = reverseString
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
